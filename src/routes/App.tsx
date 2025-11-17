2286da40368d8c368962f57f40401886a2ff6abb
inmd1 <lyw514549@gmail.com>
2025-11-17 23:54:58 +0900

    메인 모토 삽입

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
                     backgroundRepeat: "no-repeat"
                 }}
             >
-                
+
                 <header className="relative z-10">
                     <nav className="flex items-center justify-between p-6">
                         <div className="text-white text-2xl font-bold">
@@ -45,6 +45,15 @@ export default function App() {
                     className="absolute inset-0"
                     style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
                 />
+                <div
+                    className="absolute inset-0 flex items-center justify-center"
+                >
+                    <div className="grid grid-cols-1 text-white text-center">
+                        <p className="text-[1.6em]">“Where integrity meets innovation.”</p>
+                        <p className="text-[3.5em] font-bold">원칙이 혁신과 만나는 곳.</p>
+                        <p className="text-[2em] font-medium">Market Insight Lab 이 함께합니다.</p>
+                    </div>
+                </div>
 
             </section>
         </>
