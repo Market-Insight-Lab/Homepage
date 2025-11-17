e23cca95f12df568db91cecf4113cdb6d149afe9
inmd1 <lyw514549@gmail.com>
2025-11-17 23:42:49 +0900

    사이트 이미지 삽입 및 코드 시작

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,3 +1,52 @@
+import main2140 from "../asset/main2140.jpg";
+
 export default function App() {
-    return <div>App Component</div>;
+    return (
+        <>
+            <section
+                className="h-screen w-full"
+                style={{
+                    backgroundImage: `url(${main2140})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat"
+                }}
+            >
+                
+                <header className="relative z-10">
+                    <nav className="flex items-center justify-between p-6">
+                        <div className="text-white text-2xl font-bold">
+                            Market Insight Lab
+                        </div>
+                        <div className="font-bold">
+                            <a
+                                href="/"
+                                className="text-white mx-4 hover:underline"
+                            >
+                                Home
+                            </a>
+                            <a
+                                href="#"
+                                className="text-white mx-4 hover:underline"
+                            >
+                                About
+                            </a>
+                            <a
+                                href="https://github.com/Market-Insight-Lab"
+                                className="text-white mx-4 hover:underline"
+                            >
+                                GITHUB
+                            </a>
+                        </div>
+                    </nav>
+                </header>
+                {/* 어두운 오버레이 */}
+                <div
+                    className="absolute inset-0"
+                    style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }}
+                />
+
+            </section>
+        </>
+    );
 }
